da030bf58ba2aeb576a31b0132c01420d1c3aa1d
bruno <bruno214215216217218@gmail.com>
2025-01-15 20:17:17 -0300

    initial commit

---
+++ src/main/java/com/estudo/mongo/Api_StudentManager/ApiStudentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.mongo.Api_StudentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStudentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiStudentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudo/mongo/Api_StudentManager/ApiStudentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.mongo.Api_StudentManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStudentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
