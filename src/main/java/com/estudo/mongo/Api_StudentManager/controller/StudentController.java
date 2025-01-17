67ca63b9a4bcffa41927846c16ba3f00bdd4fec9
bruno <bruno214215216217218@gmail.com>
2025-01-17 20:23:25 -0300

    feat: adiciona ProdutoController para os endpoints REST

---
+++ src/main/java/com/estudo/mongo/Api_StudentManager/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.estudo.mongo.Api_StudentManager.controller;
+
+import com.estudo.mongo.Api_StudentManager.service.StudentService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+}
